b157df80203c9a7857f8a87c225a0c9a9f785428
Zeshan Tawa <zeshan.tawa98@gmail.com>
2023-02-23 19:35:41 +0000

    Complete mathPow assignment

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,6 +20,6 @@ public class Lab {
      * @return return a^b using the pow() method of the Math class.
      */
     public double mathPow(double a, double b){
-        return 0;
+        return Math.pow(a, b);
     }
 }
